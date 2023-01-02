63816b337c7aae06843dc30261536b938e0bd2d2
Golden Chimusinde <goldenchimsy@gmail.com>
2023-01-02 10:17:36 +0200

    Initialised LoginView

---
+++ PinSample/LoginViewController.swift
@@ -0,0 +1,15 @@
+//
+//  LoginViewController.swift
+//  PinSample
+//
+//  Created by Golden Chimusinde on 2023/01/02.
+//  Copyright © 2023 Udacity. All rights reserved.
+//
+
+import UIKit
+
+class LoginViewController: UIViewController {
+    
+}
+
+
