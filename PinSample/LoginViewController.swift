d9134afe85af960f41d6c6fbabf0bbdf9579eb97
Golden Chimusinde <goldenchimsy@gmail.com>
2023-01-02 12:57:22 +0200

    Completed Setting Up UI for Login Screen

--- PinSample/LoginViewController.swift
+++ PinSample/LoginViewController.swift
@@ -10,6 +10,25 @@ import UIKit
 
 class LoginViewController: UIViewController {
     
+    @IBOutlet weak var emailTextField: UITextField!
+    @IBOutlet weak var passwordTextField: UITextField!
+    @IBOutlet weak var loginButton: UIButton!
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    
+        print("\nℹ️ PinSample:  View Did Load \n✅")
+
+    }
+    
+    
+    
+    @IBAction func loginButtonTapped(_ sender: Any) {
+        print("\nℹ️ PinSample:  Did Press Login \n✅")
+    }
+    
+    
 }
 
 
