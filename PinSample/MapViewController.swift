73553694ada3cc366e29d0d510c08c43bf29e2af
Golden Chimusinde <goldenchimsy@gmail.com>
2023-01-08 10:52:48 +0200

    Initialised Map Controller

---
+++ PinSample/MapViewController.swift
@@ -0,0 +1,14 @@
+//
+//  MapViewController.swift
+//  PinSample
+//
+//  Created by Golden Chimusinde on 2023/01/08.
+//  Copyright © 2023 Udacity. All rights reserved.
+//
+
+import UIKit
+import MapKit
+
+class MapViewController: UIViewController {
+    
+}
