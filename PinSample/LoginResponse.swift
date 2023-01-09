dc83693c19835eb1ee656c55180ad1b66c0c5579
Golden Chimusinde <goldenchimsy@gmail.com>
2023-01-09 11:50:34 +0200

    Resolved Application Responses

---
+++ PinSample/LoginResponse.swift
@@ -0,0 +1,24 @@
+//
+//  LoginResponse.swift
+//  PinSample
+//
+//  Created by Golden Chimusinde on 2023/01/09.
+//  Copyright © 2023 Udacity. All rights reserved.
+//
+
+import Foundation
+
+struct LoginResponse: Codable {
+    let account: Account
+    let session: Session
+}
+
+struct Account: Codable {
+    let registered: Bool
+    let key: String
+}
+
+struct Session: Codable {
+    let id: String
+    let expiration: String
+}
