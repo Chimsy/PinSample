a78321877b982e478d2c6d2221196195b45e7875
Golden Chimusinde <goldenchimsy@gmail.com>
2023-01-09 11:46:17 +0200

    Initialised App Data

---
+++ PinSample/AppData.swift
@@ -0,0 +1,14 @@
+//
+//  AppData.swift
+//  PinSample
+//
+//  Created by Golden Chimusinde on 2023/01/09.
+//  Copyright © 2023 Udacity. All rights reserved.
+//
+
+import Foundation
+
+struct AppData {
+    static var studentLocations: [StudentLocation] = []
+    static var userId: String = ""
+}
